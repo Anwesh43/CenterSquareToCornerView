d64c8d366b9b363c2040e4f6dce20003aaa2bc9d
Anwesh43 <anweshthecool0@gmail.com>
2020-08-07 02:00:05 +0530

    adding constants and libraries

--- centersqtocornerview/src/main/java/com/anwesh/uiprojects/centersqtocornerview/CenterSqToCornerView.kt
+++ centersqtocornerview/src/main/java/com/anwesh/uiprojects/centersqtocornerview/CenterSqToCornerView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.centersqtocornerview
 /**
  * Created by anweshmishra on 07/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val colors : Array<String> = arrayOf("#3F51B5", "#FFEB3B", "#4CAF50", "#F44336", "#2196F3")
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 5.6f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
