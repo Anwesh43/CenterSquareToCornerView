e82d7f4ffe65de711e6163692e981a416ac7f095
Anwesh43 <anweshthecool0@gmail.com>
2020-08-07 02:11:43 +0530

    adding CenterToSqCornerView class

--- centersqtocornerview/src/main/java/com/anwesh/uiprojects/centersqtocornerview/CenterSqToCornerView.kt
+++ centersqtocornerview/src/main/java/com/anwesh/uiprojects/centersqtocornerview/CenterSqToCornerView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawCSTCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawCenterSqToCorner(scale, w, h, paint)
 }
+
+class CenterToSqCornerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
